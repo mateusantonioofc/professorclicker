b1ab4f5c1ffc1f9e042b460b2281357bfc6bfe58
Bruno Gustavo <fagundesbruno163@gmail.com>
2025-09-22 18:30:36 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -11,8 +11,9 @@ const professores = {
     tetianao: { nome: "Teti Anão", preco: 300, bonus: 3, img: "assets/cabibara_2.png", background: "url('assets/anao.webp')" },
     tetisupremo: { nome: "Teti Supremo", preco: 1500, bonus: 6, img: "assets/cabibara_3.jpg", background: "url('assets/sala.jpg')" },
     FelipeBase: { nome: "Felipe O Físico", preco: 2500, bonus: 7, img: "assets/FelipeBase.jpeg", background: "url('assets/Fisica.jpg')" },
-    silviogoat: { nome: "Silvio Goat", preco: 5000, bonus: 9, img: "assets/Silviogoat.jpeg", background: "url('assets/ibura.jpg')" },
-    silviofurry: { nome: "Silvio Furry", preco: 10000, bonus: 14, img: "assets/silviogoatfurry.png", background: "url('assets/academia.jpg')" },
+ Richardson: { nome: "Master Rick", preco: 7500, bonus: 11, img: "assets/FelipeBase.jpeg", background: "url('assets/Fisica.jpg')" },
+    silviogoat: { nome: "Silvio Goat", preco: 1000, bonus: 13, img: "assets/Silviogoat.jpeg", background: "url('assets/ibura.jpg')" },
+    silviofurry: { nome: "Silvio Furry", preco: 30000, bonus: 13, img: "assets/silviogoatfurry.png", background: "url('assets/academia.jpg')" },
     silviofurryshiny: { nome: "Silvio Furry Shiny", preco: 100000000000000, bonus: 99999, img: "assets/silviofurryshiny.png", background: "url('assets/City.jpg')" }
 };
 
