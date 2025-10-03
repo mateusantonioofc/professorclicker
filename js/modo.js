6c8fda1b82b66db5bfdd5fc49ac1a6c910fb6ac7
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2025-10-03 11:52:06 -0300

    Update modo.js

--- js/modo.js
+++ js/modo.js
@@ -3,8 +3,3 @@ function convidado() {
     localStorage.setItem("tipo_usuario", "convidado");
     window.location.href = "../game.html";
 }
-
-function login() {
-    localStorage.clear();
-    window.location.href = "../login.html";
-}
