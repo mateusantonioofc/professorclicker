73fa4f0ec482eb0dac3f49d4d7fae99270e324c1
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2025-10-03 15:34:22 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ const username = localStorage.getItem("nickname");
 let musicaIniciada = false;
 
 const professores = {
-    Tetianao: { nome: "Teti Anão", preco: 50, bonus: 3, img: "assets/professores/teti_anao.png", background: "url('assets/backgrounds/montanha.jpg')" },
-    Tetisupremo: { nome: "Teti Supremo", preco: 300, bonus: 5, img: "assets/professores/teti.png", background: "url('assets/backgrounds/sala.jpg')" },
-    Tetimulher: { nome: "Teti Mulher", preco: 2500, bonus: 2, img: "assets/professores/teti_mulher.png", background: "url('assets/backgrounds/cozinha.jpg')" },
+    Tetianao: { nome: "Teti Anão", preco: 50, bonus: 2, img: "assets/professores/teti_anao.png", background: "url('assets/backgrounds/montanha.jpg')" },
+    Tetisupremo: { nome: "Teti Supremo", preco: 300, bonus: 3, img: "assets/professores/teti.png", background: "url('assets/backgrounds/sala.jpg')" },
+    Tetimulher: { nome: "Teti Mulher", preco: 2500, bonus: 5, img: "assets/professores/teti_mulher.png", background: "url('assets/backgrounds/cozinha.jpg')" },
     FelipeBase: { nome: "Felipe O Grego τ", preco: 5000, bonus: 7, img: "assets/professores/felipe.png", background: "url('assets/backgrounds/fisica.jpg')" },
     Sheyla: { nome: "Dom Sheyla II", preco: 10000, bonus: 9, img: "assets/professores/sheyla.png", background: "url('assets/backgrounds/surubanco.jpg')" },
     Glauco: { nome: "Mr.Glauco", preco: 20000, bonus: 11, img: "assets/professores/glauco.png", background: "url('assets/backgrounds/fenda.jpg')" },
