eb71a5878edd9fbae100fd64833373fba7b0b800
Mateu <mateusantonioofc@gmail.com>
2025-10-17 00:25:13 -0300

    hihihi

--- js/data/professores.js
+++ js/data/professores.js
@@ -2,30 +2,30 @@ const ASSETS_PROFESSORES_PATH = "assets/professores/";
 const ASSETS_BACKGROUNDS_PATH = "assets/backgrounds/";
 
 export const PROFESSORES = {
-  teti_anao: {
-    id: "teti_anao",
-    nome: "Teti Anão",
+  dom_sheyla_ii: {
+    id: "dom_sheyla_ii",
+    nome: "Dom Sheyla II",
     preco: 50,
     bonus: 2,
-    img: ASSETS_PROFESSORES_PATH + "teti_anao.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}montanha.jpg')`,
+    img: ASSETS_PROFESSORES_PATH + "sheyla.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}surubanco.jpg')`,
   },
-  teti_supremo: {
-    id: "teti_supremo",
-    nome: "Teti Supremo",
+  mr_glauco: {
+    id: "mr_glauco",
+    nome: "Mr. Glauco",
     preco: 250,
     bonus: 3,
-    img: ASSETS_PROFESSORES_PATH + "teti.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}sala.jpg')`
+    img: ASSETS_PROFESSORES_PATH + "glauco.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}fenda.jpg')`,
   },
-  teti_mulher: {
-    id: "teti_mulher",
-    nome: "Teti Mulher",
+  master_rick: {
+    id: "master_rick",
+    nome: "Master Rick",
     preco: 1500,
     bonus: 4,
-    img: ASSETS_PROFESSORES_PATH + "teti_mulher.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}cozinha.jpg')`,
-    autoClickIntervalo: 5000
+    img: ASSETS_PROFESSORES_PATH + "richardson.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}programa.jpg')`,
+    autoClickIntervalo: 4000
   },
   felipe_grego: {
     id: "felipe_grego",
@@ -34,61 +34,61 @@ export const PROFESSORES = {
     bonus: 7,
     img: ASSETS_PROFESSORES_PATH + "felipe.png",
     background: `url('${ASSETS_BACKGROUNDS_PATH}fisica.jpg')`,
-    autoClickIntervalo: 4500
+    autoClickIntervalo: 3750
   },
-  dom_sheyla_ii: {
-    id: "dom_sheyla_ii",
-    nome: "Dom Sheyla II",
+  teti_anao: {
+    id: "teti_anao",
+    nome: "Teti Anão",
     preco: 9000,
     bonus: 10,
-    img: ASSETS_PROFESSORES_PATH + "sheyla.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}surubanco.jpg')`,
-    autoClickIntervalo: 4000
-  },
-  mr_glauco: {
-    id: "mr_glauco",
-    nome: "Mr. Glauco",
-    preco: 18000,
-    bonus: 12,
-    img: ASSETS_PROFESSORES_PATH + "glauco.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}fenda.jpg')`,
+    img: ASSETS_PROFESSORES_PATH + "teti_anao.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}montanha.jpg')`,
     autoClickIntervalo: 3500
   },
-  master_rick: {
-    id: "master_rick",
-    nome: "Master Rick",
+  teti_supremo: {
+    id: "teti_supremo",
+    nome: "Teti Supremo",
+    preco: 18000,
+    bonus: 12,
+    img: ASSETS_PROFESSORES_PATH + "teti.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}sala.jpg')`,
+    autoClickIntervalo: 3200
+  },
+  teti_mulher: {
+    id: "teti_mulher",
+    nome: "Teti Mulher",
     preco: 40000,
     bonus: 15,
-    img: ASSETS_PROFESSORES_PATH + "richardson.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}programa.jpg')`,
-    autoClickIntervalo: 3000
+    img: ASSETS_PROFESSORES_PATH + "teti_mulher.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}cozinha.jpg')`,
+    autoClickIntervalo: 2800
   },
-  silvio_goat: {
-    id: "silvio_goat",
-    nome: "Silvio Goat",
+  amaro: {
+    id: "amaro",
+    nome: "Amaro Bezerra",
     preco: 60000,
-    bonus: 18,
-    img: ASSETS_PROFESSORES_PATH + "silvio.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}ibura.jpg')`,
-    autoClickIntervalo: 2500
+    bonus: 17,
+    img: ASSETS_PROFESSORES_PATH + "amaronormal.jpeg",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}natureza.jpeg')`,
+    autoClickIntervalo: 2000
   },
-  silvio_furry: {
-    id: "silvio_furry",
-    nome: "Silvio Furry",
+  amaro_prime: {
+    id: "amaro_prime",
+    nome: "Amaro Prime",
     preco: 80000,
-    bonus: 22,
-    img: ASSETS_PROFESSORES_PATH + "silvio_furry.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}academia.jpg')`,
-    autoClickIntervalo: 2250
+    bonus: 20,
+    img: ASSETS_PROFESSORES_PATH + "amaro.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}prime.jpeg')`,
+    autoClickIntervalo: 1780
   },
-  rejane_latin: {
-    id: "rejane_latin",
-    nome: "Rejane Latin",
+  dani: {
+    id: "dani",
+    nome: "Dani",
     preco: 120000,
     bonus: 26,
-    img: ASSETS_PROFESSORES_PATH + "rejane.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}biblioteca.jpg')`,
-    autoClickIntervalo: 2000
+    img: ASSETS_PROFESSORES_PATH + "dani.jpg",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}mapa.webp')`,
+    autoClickIntervalo: 1600
   },
   luana_filosofa: {
     id: "luana_filosofa",
@@ -97,7 +97,7 @@ export const PROFESSORES = {
     bonus: 30,
     img: ASSETS_PROFESSORES_PATH + "luana.jpeg",
     background: `url('${ASSETS_BACKGROUNDS_PATH}pontanegra.webp')`,
-    autoClickIntervalo: 1500
+    autoClickIntervalo: 1550
   },
   luana_sociologa: {
     id: "luana_sociologa",
@@ -106,7 +106,7 @@ export const PROFESSORES = {
     bonus: 34,
     img: ASSETS_PROFESSORES_PATH + "luanasocio.jpeg",
     background: `url('${ASSETS_BACKGROUNDS_PATH}pontanegra.webp')`,
-    autoClickIntervalo: 500
+    autoClickIntervalo: 1300
   },
   gabriel: {
     id: "gabriel",
@@ -115,7 +115,7 @@ export const PROFESSORES = {
     bonus: 38,
     img: ASSETS_PROFESSORES_PATH + "gabriel.jpg",
     background: `url('${ASSETS_BACKGROUNDS_PATH}academiagta.webp')`,
-    autoClickIntervalo: 400,
+    autoClickIntervalo: 1000,
   },
   xandinho: {
     id: "xandinho",
@@ -124,16 +124,16 @@ export const PROFESSORES = {
     bonus: 42,
     img: ASSETS_PROFESSORES_PATH + "xandinho.jpg",
     background: `url('${ASSETS_BACKGROUNDS_PATH}deserto.png')`,
-    autoClickIntervalo: 350
+    autoClickIntervalo: 800
   },
-  dani: {
-    id: "dani",
-    nome: "Dani",
+  rejane_latin: {
+    id: "rejane_latin",
+    nome: "Rejane Latin",
     preco: 350000,
     bonus: 45,
-    img: ASSETS_PROFESSORES_PATH + "dani.jpg",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}mapa.webp')`,
-    autoClickIntervalo: 300
+    img: ASSETS_PROFESSORES_PATH + "rejane.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}biblioteca.jpg')`,
+    autoClickIntervalo: 600
   },
   albino: {
     id: "albino",
@@ -142,26 +142,26 @@ export const PROFESSORES = {
     bonus: 50,
     img: ASSETS_PROFESSORES_PATH + "albino.jpg",
     background: `url('${ASSETS_BACKGROUNDS_PATH}caverna.png')`,
-    autoClickIntervalo: 270
+    autoClickIntervalo: 400
   },
-  amaro: {
-    id: "amaro",
-    nome: "Amaro Bezerra",
+  silvio_goat: {
+    id: "silvio_goat",
+    nome: "Silvio Goat",
     preco: 450000,
-    bonus: 55,
-    img: ASSETS_PROFESSORES_PATH + "amaronormal.jpeg",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}natureza.jpeg')`,
-    autoClickIntervalo: 260
+    bonus: 50,
+    img: ASSETS_PROFESSORES_PATH + "silvio.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}ibura.jpg')`,
+    autoClickIntervalo: 300
   },
-  amaro_prime: {
-    id: "amaro_prime",
-    nome: "Amaro Prime",
+  silvio_furry: {
+    id: "silvio_furry",
+    nome: "Silvio Furry",
     preco: 475000,
     bonus: 60,
-    img: ASSETS_PROFESSORES_PATH + "amaro.png",
-    background: `url('${ASSETS_BACKGROUNDS_PATH}prime.jpeg')`,
-    autoClickIntervalo: 250
-  }
+    img: ASSETS_PROFESSORES_PATH + "silvio_furry.png",
+    background: `url('${ASSETS_BACKGROUNDS_PATH}academia.jpg')`,
+    autoClickIntervalo: 200
+  },
 };
 
 export function getProfessor(id) {
